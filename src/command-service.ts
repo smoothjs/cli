f502a5bcbd2b47bee66e4887e8c7528b52b397bd
AbdallahMohammed <abdallah.r660@gmail.com>
2021-03-31 13:34:03 +0200

    style: 💄 version

--- src/command-service.ts
+++ src/command-service.ts
@@ -11,7 +11,7 @@ export class CommandService {
   }
 
   public async create() {
-    this.commander.version('0.0.1')
+    this.commander.version('0.1.0')
 
     this.commands.forEach((command: object) => {
       this.registerCommand(command)
